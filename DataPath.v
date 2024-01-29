29ba1823157391179cade4a7de438c62fa192692
conboy <conjamalex@gmail.com>
2024-01-29 14:55:38 -0500

    Add input wires for bus multiplexer and update ALU registers

--- DataPath.v
+++ DataPath.v
@@ -3,6 +3,33 @@ module DataPath(
     input wire clear
 );
 
+// Input wires to the bus's 32-to-1 multiplexer
+wire [31:0] R0_data_out;
+wire [31:0] R1_data_out;
+wire [31:0] R2_data_out;
+wire [31:0] R3_data_out;
+wire [31:0] R4_data_out;
+wire [31:0] R5_data_out;
+wire [31:0] R6_data_out;
+wire [31:0] R7_data_out;
+wire [31:0] R8_data_out;
+wire [31:0] R9_data_out;
+wire [31:0] R10_data_out;
+wire [31:0] R11_data_out;
+wire [31:0] R12_data_out;
+wire [31:0] R13_data_out;
+wire [31:0] R14_data_out;
+wire [31:0] R15_data_out;
+wire [31:0] HI_data_out;
+wire [31:0] LO_data_out;
+wire [31:0] Z_HI_data_out;
+wire [31:0] Z_LO_data_out;
+wire [31:0] PC_data_out;
+wire [31:0] MDR_data_out;
+wire [31:0] INPORT_data_out;
+wire [31:0] C_sign_extended;
+
+// TODO: Input wires to the bus's 32-to-5 multiplexer (for enable signals)
 
 // PROCESSOR STATE REGISTERS
 register PC(clock, clear, PCin, bus_contents, PC_data_out); // Program Counter
@@ -38,7 +65,8 @@ register R15(clock, clear, R15in, bus_contents, R15_data_out);
 
 // ALU REGISTERS
 register Y(clock, clear, Yin, bus_contents, Y_data_out); // Y Register dedicated to keep the high-order word of a Multiplication product, or the Remainder of a Division operation
-register Z(clock, clear, Zin, bus_contents, Z_data_out); // Z Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
+register Z_HI(clock, clear, Z_HIin, bus_contents, Z_HI_data_out); // Z Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
+register Z_LO(clock, clear, Z_LOin, bus_contents, Z_LO_data_out); // Z Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
 
 // MEMORY STATE REGISTERS
 register MDR(clock, clear, MDRin, bus_contents, MDR_data_out); // Memory Data Register
@@ -49,12 +77,14 @@ register INPORT(clock, clear, INPORTin, INPORT_input, INPORT_data_out); // Input
 register OUTPORT(clock, clear, OUTPORTin, bus_contents, OUTPORT_data_out); // Input Port Register
 
 // TODO: Mem[0..511]<31..0>: 512 words (32 bits per word) of memory
-// TODO: I/O State
-// In.Port<31..0>: 32-bit Input Port
-// Out.Port<31..0>: 32-bit Output Port
+// TODO: I/O States
 // Run.Out: Run/halt Indicator
 // Stop.In: Stop signal
 // Reset.In: Reset signal
+
+// TODO: Adder
+
+// TODO: BUS
 
 
 endmodule
