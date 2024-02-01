7ce1fb1be33b03816aabd5d5f5ce31ef9c3d47c5
marcus-wrrn <marcuswrrn@gmail.com>
2024-02-01 14:44:37 -0500

    Changed variable names

--- encoder_32_5.v
+++ encoder_32_5.v
@@ -1,15 +1,15 @@
 module encoder_32_5(
-    input [31:0] encoder_input,
-    output reg [4:0] encoder_output
+    input [31:0] encoderInput,
+    output reg [4:0] encoderOutput
 )
 
 integer i;
 
-always @(encoder_input) begin
-    encoder_output = 5'd31;
+always @(encoderInput) begin
+    encoderOutput = 5'd31;
     for (i = 0; i <= 23; i = i + 1) begin
-        if (encoder_input[i]) begin
-            encoder_output = i;
+        if (encoderInput[i]) begin
+            encoderOutput = i;
         end
     end
 end
