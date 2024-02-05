d3ac8a7ddd21f877b582f29d85cc29851342712e
conboy <conjamalex@gmail.com>
2024-02-05 13:38:39 -0500

    Refactor datapath code to improve readability

--- phase_1/datapath.v
+++ phase_1/datapath.v
@@ -1,125 +1,129 @@
 // TODO: Finish the datapath module
 
 module datapath (
-	input wire clock,
-    input wire clear
+	input wire clk,
+    input wire clr,
+    input wire[31:0] select;
+
 );
 
-// Input wires to the bus's 32-to-1 multiplexer
-input wire [31:0] R0_data_out;
-input wire [31:0] R1_data_out;
-input wire [31:0] R2_data_out;
-input wire [31:0] R3_data_out;
-input wire [31:0] R4_data_out;
-input wire [31:0] R5_data_out;
-input wire [31:0] R6_data_out;
-input wire [31:0] R7_data_out;
-input wire [31:0] R8_data_out;
-input wire [31:0] R9_data_out;
-input wire [31:0] R10_data_out;
-input wire [31:0] R11_data_out;
-input wire [31:0] R12_data_out;
-input wire [31:0] R13_data_out;
-input wire [31:0] R14_data_out;
-input wire [31:0] R15_data_out;
-input wire [31:0] HI_data_out;
-input wire [31:0] LO_data_out;
-input wire [31:0] Z_HI_data_out;
-input wire [31:0] Z_LO_data_out;
-input wire [31:0] PC_data_out;
-input wire [31:0] MDR_data_out;
-input wire [31:0] INPORT_data_out;
-input wire [31:0] C_sign_extended;
+// Register enable signals
+wire r0_enable;
+wire r1_enable;
+wire r2_enable;
+wire r3_enable;
+wire r4_enable;
+wire r5_enable;
+wire r6_enable;
+wire r7_enable;
+wire r8_enable;
+wire r9_enable;
+wire r10_enable;
+wire r11_enable;
+wire r12_enable;
+wire r13_enable;
+wire r14_enable;
+wire r15_enable;
+wire y_enable;
+wire hi_enable;
+wire lo_enable;
+wire zlo_enable;
+wire zhi_enable;
+wire pc_enable;
+wire mdr_enable;
+wire ir_enable;
 
-// TODO: Input wires to the bus's 32-to-5 multiplexer (for enable signals)
+// Register output data wires (connect to the bus)
+wire[31:0] r0_out = 0;
+wire[31:0] r1_out = 0;
+wire[31:0] r2_out = 0;
+wire[31:0] r3_out = 0;
+wire[31:0] r4_out = 0;
+wire[31:0] r5_out = 0;
+wire[31:0] r6_out = 0;
+wire[31:0] r7_out = 0;
+wire[31:0] r8_out = 0;
+wire[31:0] r9_out = 0;
+wire[31:0] r10_out = 0;
+wire[31:0] r11_out = 0;
+wire[31:0] r12_out = 0;
+wire[31:0] r13_out = 0;
+wire[31:0] r14_out = 0;
+wire[31:0] r15_out = 0;
+wire[31:0] pc_out = 0;
+wire[31:0] hi_out = 0;
+wire[31:0] lo_out = 0;
+wire[31:0] zhi_out = 0;
+wire[31:0] mdr_out = 0;
+wire[31:0] zlo_out = 0;
+wire[31:0] inport_out = 0;
+wire[31:0] c_sign_extended_out = 0;
 
-// PROCESSOR STATE REGISTERS
-register PC(clock, clear, PCin, bus_contents, PC_data_out); // Program Counter
-register IR(clock, clear, IRin, bus_contents, IR_data_out); // Instruction Register
-register HI(clock, clear, HIin, bus_contents, HI_data_out); // HI Register dedicated to keep the high-order word of a Multiplication product, or the Remainder of a Division operation
-register LO(clock, clear, LOin, bus_contents, LO_data_out); // LO Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
+// Instantiate register modules
+reg_32_bit r0(clk, clr, r0_enable, bus, r0_out);
+reg_32_bit r1(clk, clr, r1_enable, bus, r1_out);
+reg_32_bit r2(clk, clr, r2_enable, bus, r2_out);
+reg_32_bit r3(clk, clr, r3_enable, bus, r3_out);
+reg_32_bit r4(clk, clr, r4_enable, bus, r4_out);
+reg_32_bit r5(clk, clr, r5_enable, bus, r5_out);
+reg_32_bit r6(clk, clr, r6_enable, bus, r6_out);
+reg_32_bit r7(clk, clr, r7_enable, bus, r7_out);
+reg_32_bit r8(clk, clr, r8_enable, bus, r8_out);
+reg_32_bit r9(clk, clr, r9_enable, bus, r9_out);
+reg_32_bit r10(clk, clr, r10_enable, bus, r10_out);
+reg_32_bit r11(clk, clr, r11_enable, bus, r11_out);
+reg_32_bit r12(clk, clr, r12_enable, bus, r12_out);
+reg_32_bit r13(clk, clr, r13_enable, bus, r13_out);
+reg_32_bit r14(clk, clr, r14_enable, bus, r14_out);
+reg_32_bit r15(clk, clr, r15_enable, bus, r15_out);
 
-// General-Purpose Register
-register R0(clock, clear, R0in, bus_contents, R0_data_out);
-register R1(clock, clear, R1in, bus_contents, R1_data_out);
-register R2(clock, clear, R2in, bus_contents, R2_data_out);
-register R3(clock, clear, R3in, bus_contents, R3_data_out);
-register R4(clock, clear, R4in, bus_contents, R4_data_out);
-register R5(clock, clear, R5in, bus_contents, R5_data_out);
-register R6(clock, clear, R6in, bus_contents, R6_data_out);
-register R7(clock, clear, R7in, bus_contents, R7_data_out);
+reg_32_bit y(clk, clr, y_enable, bus, y_out);
+reg_32_bit hi(clk, clr, hi_enable, bus, hi_out);
+reg_32_bit lo(clk, clr, lo_enable, bus, lo_out);
+reg_32_bit zlo(clk, clr, zlo_enable, bus, zlo_out);
+reg_32_bit zhi(clk, clr, zhi_enable, bus, zhi_out);
+reg_32_bit pc(clk, clr, pc_enable, bus, pc_out);
+reg_32_bit mdr(clk, clr, mdr_enable, bus, mdr_out);
+reg_32_bit ir(clk, clr, ir_enable, bus, ir_out);
 
-// Return Value Registers
-register R8(clock, clear, R8in, bus_contents, R8_data_out);
-register R9(clock, clear, R9in, bus_contents, R9_data_out);
-
-// Argument Registers
-register R10(clock, clear, R10in, bus_contents, R10_data_out);
-register R11(clock, clear, R11in, bus_contents, R11_data_out);
-register R12(clock, clear, R12in, bus_contents, R12_data_out);
-register R13(clock, clear, R13in, bus_contents, R13_data_out);
-
-// Stack Pointer Register (SP) register
-register R14(clock, clear, R14in, bus_contents, R14_data_out);
-
-// Return Address Register (RA)
-register R15(clock, clear, R15in, bus_contents, R15_data_out);  
-
-// ALU REGISTERS
-register Y(clock, clear, Yin, bus_contents, Y_data_out); // Y Register dedicated to keep the high-order word of a Multiplication product, or the Remainder of a Division operation
-register Z_HI(clock, clear, Z_HIin, bus_contents, Z_HI_data_out); // Z Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
-register Z_LO(clock, clear, Z_LOin, bus_contents, Z_LO_data_out); // Z Register dedicated to keep the low-order word of a Multiplication product, or the Quotient of a Division operation
-
-// MEMORY STATE REGISTERS
-register MDR(clock, clear, MDRin, bus_contents, MDR_data_out); // Memory Data Register
-register MAR(clock, clear, MARin, bus_contents, MAR_data_out); // Memory Address Register
-
-// I/O UNIT REGISTERS
-register INPORT(clock, clear, INPORTin, INPORT_input, INPORT_data_out); // Input Port Register
-register OUTPORT(clock, clear, OUTPORTin, bus_contents, OUTPORT_data_out); // Input Port Register
-
-// TODO: Mem[0..511]<31..0>: 512 words (32 bits per word) of memory
-// TODO: I/O States
-// Run.Out: Run/halt Indicator
-// Stop.In: Stop signal
-// Reset.In: Reset signal
-
-
-// TODO: BUS
-
-// Initialize 32 to 1 multiplexer to select the output to send out to the bus
-bus_mux BusMux(
-    .BusMuxIn_R0(R0_data_out),
-    .BusMuxIn_R1(R1_data_out), 
-    .BusMuxIn_R2(R2_data_out),
-    .BusMuxIn_R3(R3_data_out),
-    .BusMuxIn_R4(R4_data_out),
-    .BusMuxIn_R5(R5_data_out),
-    .BusMuxIn_R6(R6_data_out),
-    .BusMuxIn_R7(R7_data_out),
-    .BusMuxIn_R8(R8_data_out),
-    .BusMuxIn_R9(R9_data_out),
-    .BusMuxIn_R10(R10_data_out),
-    .BusMuxIn_R11(R11_data_out),
-    .BusMuxIn_R12(R12_data_out),
-    .BusMuxIn_R13(R13_data_out),
-    .BusMuxIn_R14(R14_data_out),
-    .BusMuxIn_R15(R15_data_out),
-    .BusMuxIn_HI(HI_data_out),
-    .BusMuxIn_LO(LO_data_out),
-    .BusMuxIn_Z_HI(Z_HI_data_out),
-    .BusMuxIn_Z_LO(Z_LO_data_out),
-    .BusMuxIn_PC(PC_data_out),
-    .BusMuxIn_MDR(MDR_data_out),
-    .BusMuxIn_INPORT(INPORT_data_out),
-    .C_sign_extended(C_sign_extended),
-    .BusMuxOut(bus_contents),
-    .select_signal(bus_encoder_signal)
+// Instantiate Bus Mux
+bus_mux_32_to_1 bus_mux(
+    .bus_mux_out(bus),
+    .select_encoded(select_encoded),
+    .bus_mux_in_r0(r0_out),
+    .bus_mux_in_r1(r1_out),
+    .bus_mux_in_r2(r2_out),
+    .bus_mux_in_r3(r3_out),
+    .bus_mux_in_r4(r4_out),
+    .bus_mux_in_r5(r5_out),
+    .bus_mux_in_r6(r6_out),
+    .bus_mux_in_r7(r7_out),
+    .bus_mux_in_r8(r8_out),
+    .bus_mux_in_r9(r9_out),
+    .bus_mux_in_r10(r10_out),
+    .bus_mux_in_r11(r11_out),
+    .bus_mux_in_r12(r12_out),
+    .bus_mux_in_r13(r13_out),
+    .bus_mux_in_r14(r14_out),
+    .bus_mux_in_r15(r15_out),
+    .bus_mux_in_hi(hi_out),
+    .bus_mux_in_lo(lo_out),
+    .bus_mux_in_zhi(zhi_out),
+    .bus_mux_in_zlo(zlo_out),
+    .bus_mux_in_pc(pc_out),
+    .bus_mux_in_mdr(mdr_out),
+    // TODO: Verify the following connections
+    .bus_mux_in_inport(inport_out),
+    .bus_mux_in_c_sign_extended(c_sign_extended_out)
 );
 
-// TODO: Initialize 32 to 5 multiplexer to select the enable signal to send out to the 32 to 1 multiplexer
+// Instantiate Select Signal Encoder and encoded select signal
+wire [4:0] select_encoded;
+encoder_32_to_5 select_encoder(
+    .encoder_in(select),
+    .encoder_out(select_encoded)
+);
 
-// TODO: Initialize ALU
-
+// TODO: Instantiate ALU
 
 endmodule
