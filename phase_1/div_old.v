dc10114198e69e039e481f4f030131ad7fd55783
marcus-wrrn <marcuswrrn@gmail.com>
2024-02-25 09:59:10 -0500

    Fixed old division logic

--- phase_1/div_old.v
+++ phase_1/div_old.v
@@ -19,20 +19,24 @@ module div(input [31:0] in_a, in_b, output reg[63:0] out);
                 a = {a[30:0], q[31]};
                 q[31:1] = q[30:0];
 
-                if (a[31]) begin
-                    a = a + m;    
-                    q[0] = 0;
-                end 
-                else begin
-                    a = a - m;
+                if(a[31] == 0) begin
+					a = a - m;
+                end
+				else begin
+					a = a + m;
+                end
+                if(a[31] == 0) begin
                     q[0] = 1;
+                end
+                else begin
+                    q[0] = 0;
                 end
             end
 
-            if (a[31]) begin
+            if (a[31] == 1) begin
                 a = a + m;
             end
-            if (in_a[31]) begin
+            if (in_a[31] == 1) begin
                 q = -q;
             end
 
