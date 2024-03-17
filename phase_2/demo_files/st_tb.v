0d3f9637681af029afd9a3d4a0074326514a8f2e
conboy <conjamalex@gmail.com>
2024-03-17 18:28:11 -0400

    st case 1 and 2 work

--- phase_2/demo_files/st_tb.v
+++ phase_2/demo_files/st_tb.v
@@ -45,14 +45,21 @@ wire [31:0] zlo_data;
 
 // FSM signals
 parameter start = 4'b0000; 
-parameter T0 = 4'b0001; 
-parameter T1 = 4'b0010; 
-parameter T2 = 4'b0011; 
-parameter T3 = 4'b0100; 
-parameter T4 = 4'b0101; 
-parameter T5 = 4'b0110; 
-parameter T6 = 4'b0111; 
-parameter T7 = 4'b1000;
+parameter T0_ldi = 4'b0001; 
+parameter T1_ldi = 4'b0010; 
+parameter T2_ldi = 4'b0011; 
+parameter T3_ldi = 4'b0100; 
+parameter T4_ldi = 4'b0101; 
+parameter T5_ldi = 4'b0110; 
+parameter T0_st = 4'b0111; 
+parameter T1_st = 4'b1000;
+parameter T2_st = 4'b1001;
+parameter T3_st = 4'b1010;
+parameter T4_st = 4'b1011;
+parameter T5_st = 4'b1100;
+parameter T6_st = 4'b1101;
+parameter T7_st = 4'b1110;
+parameter T8_st = 4'b1111;
 
 reg	[3:0] present_state = start;
 
@@ -143,14 +150,21 @@ begin
     if (toggle == 1)  // Check if toggle is set to change the state
     begin
         case (present_state)
-            start       : present_state = T0;
-            T0  		: present_state = T1;
-            T1  		: present_state = T2;
-            T2  		: present_state = T3;
-            T3  		: present_state = T4;
-            T4  		: present_state = T5;
-            T5  		: present_state = T6;
-            T6          : present_state = T7;
+            start       : present_state = T0_ldi;
+            T0_ldi  		: present_state = T1_ldi;
+            T1_ldi  		: present_state = T2_ldi;
+            T2_ldi  		: present_state = T3_ldi;
+            T3_ldi  		: present_state = T4_ldi;
+            T4_ldi  		: present_state = T5_ldi;
+            T5_ldi  		: present_state = T0_st;
+            T0_st          : present_state = T1_st;
+            T1_st          : present_state = T2_st;
+            T2_st          : present_state = T3_st;
+            T3_st          : present_state = T4_st;
+            T4_st          : present_state = T5_st;
+            T5_st          : present_state = T6_st;
+            T6_st          : present_state = T7_st;
+            T7_st          : present_state = T8_st;
         endcase
     end
 end
@@ -173,48 +187,91 @@ begin
 		end
 		
 		// present_state: 1
-		T0: begin
+		T0_ldi: begin
 			pc_out <= 1; mar_enable <= 1; pc_increment <= 1; 
 			#20 mar_enable <= 0; pc_increment <= 0; z_enable <= 1;
 			#20 pc_out <= 0; z_enable <= 0;
 		end
 
 		// present_state: 2
-		T1: begin
-			// case 1: m_data_in <= 32'b00010_0001_0000_0000000000010000111;
+		T1_ldi: begin
+			// ldi instruction
 			read <= 1; mdr_enable <= 1; pc_enable <= 1; zlo_out <= 1;
 			#20 read <= 0; mdr_enable <= 0; pc_enable <= 0; zlo_out <= 0;
 		end
 
 		// present_state: 3
-		T2: begin
+		T2_ldi: begin
 			mdr_out <= 1; ir_enable <= 1; 
 			#20 mdr_out<= 0; ir_enable <= 0;
 		end
 
 		// present_state: 4
-        // 
-		T3: begin	
+		T3_ldi: begin	
 			grb <= 1; ba_out <= 1; y_enable <= 1;
 			#20 grb <= 0; ba_out <= 0; y_enable <= 0;
 		end
 
 		// present_state: 5
-		T4: begin
-			c_sign_extended_out <= 1;  z_enable <= 1;   // ADD c_sign_extended_out + grb
+		T4_ldi: begin	
+			c_sign_extended_out <= 1;  z_enable <= 1;
 			#20 c_sign_extended_out <= 0; z_enable <= 0;
 		end
 
 		// present_state: 6
-		T5: begin
+		T5_ldi: begin
+			zlo_out <= 1; gra <= 1; r_in <= 1;
+			#20 zlo_out <= 0; gra <= 0;  r_in <= 0;
+		end
+
+        // present_state: 7
+        T0_st: begin
+            pc_out <= 1; mar_enable <= 1; pc_increment <= 1; 
+			#20 mar_enable <= 0; pc_increment <= 0; z_enable <= 1;
+			#20 pc_out <= 0; z_enable <= 0;
+        end
+
+        // present_state: 8
+        T1_st: begin
+			// st instruction
+			read <= 1; mdr_enable <= 1; pc_enable <= 1; zlo_out <= 1;
+			#20 read <= 0; mdr_enable <= 0; pc_enable <= 0; zlo_out <= 0;
+		end
+
+        // present_state: 9
+        T2_st: begin
+            mdr_out <= 1; ir_enable <= 1; 
+			#20 mdr_out<= 0; ir_enable <= 0;
+        end
+
+        // present_state: 10
+        T3_st: begin
+            grb <= 1; ba_out <= 1; y_enable <= 1;
+			#20 grb <= 0; ba_out <= 0; y_enable <= 0;
+        end
+
+        // present_state: 11
+        T4_st: begin
+            c_sign_extended_out <= 1;  z_enable <= 1;   // ADD c_sign_extended_out (gra) + grb
+			#20 c_sign_extended_out <= 0; z_enable <= 0;
+        end
+
+        // present_state: 12
+        T5_st: begin
 			zlo_out <= 1; mar_enable <= 1;
 			#20 zlo_out <= 0; mar_enable <= 0;
 		end
 
-        // present_state: 7
-        T6: begin
+        // present_state: 13
+        T6_st: begin
             gra <= 1; r_out <= 1; mdr_enable <= 1;
             #20 gra <= 0; r_out <= 0; mdr_enable <= 0;
+        end
+
+        // present_state: 14
+        T7_st: begin
+            ram_write <= 1;
+            #20 ram_write <= 0;
         end
 	endcase
 end
